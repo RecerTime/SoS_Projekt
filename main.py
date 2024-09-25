1ee6a070c576d037de6295decb3153610cc97709
Hugo Vennergrund Blom <Hugo.wennergrundblom@gmail.com>
2024-09-25 14:44:55 +0200

    Initial Commit

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
+import matplotlib.transforms as mtransforms
 import numpy as np
 import scipy
+
+G = float
+R = float
+C = float
+R_ratio = float
+
+def H1(s):
+    pass
+
+def H2(s):
+    pass
+
+def H3(s):
+    pass
