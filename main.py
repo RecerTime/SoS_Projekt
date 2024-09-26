26e22c34d608a72fb95bab3124aa7e76bc5a4313
Hugo Vennergrund Blom <Hugo.wennergrundblom@gmail.com>
2024-09-26 23:36:48 +0200

    LTI Functions Added

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import matplotlib.pyplot as plt
 import matplotlib.transforms as mtransforms
 import numpy as np
-import scipy
+from scipy import signal
 
 G = float
 R = float
 C = float
+k = R*C
 R_ratio = float
 
-def H1(s):
-    pass
+H1 = signal.lti([1],[-(k/G)**2, (k/G)*R_ratio, 1])
+H2 = signal.lti([1,0],[-R_ratio*k/G, 1, R_ratio*G/k])
+H3 = signal.lti([1,0,0],[-1, (G/k)*R_ratio, (G/k)**2])
 
-def H2(s):
-    pass
+transfer_functions = [H1, H2, H3]
 
-def H3(s):
-    pass
+for H in transfer_functions:
+    H.poles()
+    H.zeros()
+    H.impulse()
+    H.bode()
